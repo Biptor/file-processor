0e091c369754026b62de08a30a3756866a1bd581
Biptor <mena.victor@gmail.com>
2025-05-23 22:31:40 +0000

    parsd datetime

--- FileProcessor/Program.cs
+++ FileProcessor/Program.cs
@@ -1,15 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
 Console.WriteLine("Hello, World!");
 
 
 var linesPersonFile = new List<string>(){
-    "123, Biptor, 82-04-05",
-    "456, ChinoChino, 80-03-31"
+    "123,Biptor,82-04-05",
+    "456,ChinoChino,80-03-31"
 };
 
 var linesAddressFile = new List<string>(){
-    "calle123, Cali",
-    "calle456, San Agustin"
+    "calle123,Cali",
+    "calle456,San Agustin"
 };
 
 var outputLines = new List<string>();
@@ -22,5 +23,23 @@ for (var i = 0;  i < linesPersonFile.Count(); i++) {
 
 foreach (var output in outputLines) {
     Console.WriteLine(output);
+
+    var lineContent = output.Split(',');
+
+    Console.WriteLine(lineContent);
+
+    DateTime fecha;
+    bool exito = DateTime.TryParseExact(
+    lineContent[2],
+    "yy-MM-dd",
+    CultureInfo.InvariantCulture,
+    DateTimeStyles.None,
+    out fecha);
+
+    Console.WriteLine(fecha.ToString());
+
+    foreach (var content in lineContent){
+        var tmp = string.Concat(content, "|");
+    }
 }
 
