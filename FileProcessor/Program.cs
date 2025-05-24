04fb829769329a0b47071eda37521c98a4ffe971
Biptor <mena.victor@gmail.com>
2025-05-24 03:25:55 +0000

    Read files

--- FileProcessor/Program.cs
+++ FileProcessor/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Globalization;
+using System;
+using System.IO;
 
 var linesPersonFile = new List<string>(){
     "123,Biptor,82-04-05",
@@ -11,10 +13,13 @@ var linesAddressFile = new List<string>(){
     "calle456,San Agustin"
 };
 
+
+linesPersonFile = ReadFile("Persons.txt").ToList();
+linesAddressFile = ReadFile("Addresses.txt").ToList();
+
 var merged = MergeFilesContent(linesPersonFile, linesAddressFile);
 
 var finalContent = GenerateFileContent(merged);
-
 
 
 IEnumerable<string> MergeFilesContent(List<string> linesPersonFile,
@@ -50,10 +55,30 @@ IEnumerable<string> GenerateFileContent(IEnumerable<string> linesMerged) {
             lineContent[2] = dateOfBirth.ToString();
 
             var finalLine = string.Join('|', lineContent);
-
+            Console.WriteLine(finalLine);
             outputLines.Add(finalLine);
         }
     }
 
     return outputLines;
+}
+
+IEnumerable<string> ReadFile(string path) {
+    var lines = new List<string>();
+
+    try
+    {
+        foreach (var line in File.ReadLines(path))
+        {
+            Console.WriteLine(line);
+            lines.Add(line);
+        }
+
+        return lines;
+    }
+    catch (IOException ex)
+    {
+        Console.WriteLine($"Error al leer el archivo: {ex.Message}");
+        throw;
+    }
 }
